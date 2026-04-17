ba9234ad01eb6b56df1bf9bf71326dd63cf0205e
Blue_W <118440041+BlueW69@users.noreply.github.com>
2026-04-17 19:53:27 +0200

    Update memexec.h
    - added enum for supported types
    - fixed perfect forwarding

--- memexec/src/memexec.h
+++ memexec/src/memexec.h
@@ -7,19 +7,55 @@
 #include <windows.h>
 #include <oleauto.h>
 #include <stdexcept>
+#include <vector>
 #include <span>
 
 #pragma comment(lib, "oleaut32.lib")
 
 namespace memexec
 {
+    enum class types : std::uint8_t
+    {
+        // ------------------------- Special Cases ------------------------- //
+
+        none = 0,         // VT_EMPTY        (void)
+        void_ptr,         // byref           (PVOID)
+        boolean,          // boolVal         (VARIANT_BOOL)    
+
+        // ------------------------ Signed Integers ------------------------ //
+
+        i8,               // cVal            (char)
+        i16,              // iVal            (short)
+        i32,              // intVal | lVal   (int) | (long)
+        i64,              // llVal           (long long)
+                                     
+        // ----------------------- Unsigned Integers ----------------------- //
+
+        u8,               // bVal            (byte)
+        u16,              // uiVal           (unsigned short)
+        u32,              // uintVal | ulVal (unsigned int) | (unsigned long)
+        u64,              // ullVal          (unsigned long long)
+                                     
+        // ------------------------ Floating Points ------------------------ // 
+
+        f32,              // fltVal          (FLOAT)
+        f64               // dblVal          (DOUBLE)
+
+        // ----------------------------------------------------------------- //
+    };
+
+    struct value
+    {
+
+    };
+
     class rcg
     {
     private:
 
         void* mem_ = nullptr;
         size_t size_ = 0;
-
+        
         void cleanup()
         {
             if (mem_)
@@ -31,6 +67,15 @@ namespace memexec
         }
 
     public:
+
+        
+
+        struct function_structure
+        {
+            types return_type { };
+            std::vector<types> argument_types { };
+            std::vector<VARIANTARG*> argument_values { };
+        };
 
         rcg() {}
 
@@ -80,6 +125,11 @@ namespace memexec
             return *this;
         }
 
+        ~rcg()
+        {
+            cleanup();
+        }
+
 
         bool register_function(const std::uint8_t* code, size_t size) noexcept
         {
@@ -111,18 +161,42 @@ namespace memexec
         }
 
 
-        template <typename ReturnType, typename... Params>
-        auto get() const noexcept -> ReturnType(*)(Params...)
+        template <typename ReturnType, typename... ParamTypes>
+        auto get() const noexcept -> ReturnType(*)(ParamTypes...)
         {
-            return reinterpret_cast<ReturnType(*)(Params...)>(mem_);
+            return reinterpret_cast<ReturnType(*)(ParamTypes...)>(mem_);
         }
 
         template <typename ReturnType, typename... Params>
-        ReturnType call(Params... args) const noexcept
+        ReturnType call(Params&&... args) const noexcept
         {
             return get<ReturnType, Params...>()(std::forward<Params>(args)...);
         }
 
+
+        #if defined(_M_X64) || defined(__x86_64__)
+
+        //VARIANT assemble_and_call(function_structure& str)
+        //{
+        //    VARIANT result;
+        //
+        //    DispCallFunc(nullptr, 
+        //                 reinterpret_cast<ULONG_PTR>(mem_),
+        //                 CC_FASTCALL,
+        //                 str.return_type,
+        //                 static_cast<UINT>(str.argument_values.size()),
+        //                 str.argument_types.data(),
+        //                 str.argument_values.data(),
+        //                 &result);
+        //
+        //    return result;
+        //}
+
+
+       
+        #else
+
+        #endif
 
         size_t size() const noexcept
         {
@@ -140,10 +214,7 @@ namespace memexec
         }
 
 
-        ~rcg()
-        {
-            cleanup();
-        }
+        
     };
 
 }
